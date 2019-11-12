bb45b06bc1c38e09cae44c491e57d519f5f11332
feng_yt <fengyantao>
2019-11-12 15:24:50 +0800

    开启hystrix链路监控

--- hystrix-consumer/src/main/java/com/feng/cloud/study/HystrixConsumerApplication.java
+++ hystrix-consumer/src/main/java/com/feng/cloud/study/HystrixConsumerApplication.java
@@ -3,10 +3,15 @@ package com.feng.cloud.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 //开启hystrix
 @EnableCircuitBreaker
+//开启hystrix链路监控
+@EnableTurbine
+@EnableHystrixDashboard
 public class HystrixConsumerApplication {
 
     public static void main(String[] args) {
